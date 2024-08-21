4799b490986b9966378a0da7af0b16a345ca1301
J.C. Jones <jc@insufficient.coffee>
2024-08-21 11:18:43 -0700

    Get clustering set up with disaster recovery

--- tikv/start-cluster.sh
+++ tikv/start-cluster.sh
@@ -8,7 +8,9 @@ fi
 MY_IP="${1}"
 PD_IP="${2:-}"
 
-CLUSTER_DIR="$(mktemp -d --tmpdir="${HOME}/.tiup/")"
+if [ -z "${CLUSTER_DIR}" ]; then
+	CLUSTER_DIR="$(mktemp -d --tmpdir="${HOME}/.tiup/")"
+fi
 
 cat <<EOF >"${CLUSTER_DIR}/config-tikv.toml"
 [raftdb]
@@ -27,22 +29,31 @@ EOF
 cat <<EOF >"${CLUSTER_DIR}/config-pd.toml"
 [schedule]
 patrol-region-interval = "100ms"
+
+[replication]
+location-labels = ["zone", "rack", "host"]
+
+[replication-mode]
+replication-mode = "dr-auto-sync"
+[replication-mode.dr-auto-sync]
+label-key = "zone"
 EOF
 
 PD_FLAGS=(
-	--name=rocinante
+	--name=$(hostname)
 	--config="${CLUSTER_DIR}/config-pd.toml"
 	--data-dir="${CLUSTER_DIR}/pd/data"
-	--peer-urls=http://${MY_IP}:2380
+	--peer-urls=http://0.0.0.0:2380
 	--advertise-peer-urls=http://${MY_IP}:2380
-	--client-urls=http://${MY_IP}:2379
+	--client-urls=http://0.0.0.0:2379
 	--advertise-client-urls=http://${MY_IP}:2379
-	--initial-cluster=rocinante=http://${MY_IP}:2380
 	--log-file="${CLUSTER_DIR}/pd/log.txt"
 )
 
 if [ -n "${PD_IP}" ]; then
-	PD_CONF+="--join=${PDF_IP}:2379"
+	PD_FLAGS+=("--join=http://${PD_IP}:2379")
+else
+	PD_FLAGS+=("--initial-cluster=$(hostname)=http://${MY_IP}:2380")
 fi
 
 tiup install pd:v8.2.0 tikv:v8.2.0
@@ -54,11 +65,18 @@ PD_PID=$!
 TIKV_FLAGS=(
 	--config="${CLUSTER_DIR}/config-tikv.toml"
 	--data-dir="${CLUSTER_DIR}/tikv/data"
-	--addr="${MY_IP}:20160"
+	--addr="0.0.0.0:20160"
 	--advertise-addr="${MY_IP}:20160"
-	--status-addr="${MY_IP}:20180"
+	--status-addr="0.0.0.0:20180"
 	--pd-endpoints="http://${MY_IP}:2379"
+	--log-file="${CLUSTER_DIR}/tikv/log.txt"
 )
+
+if [ "${1}" == "10.0.0.80" ] ; then
+	TIKV_FLAGS+=("--labels=zone=slc-1,rack=1,host=proxmox")
+else
+	TIKV_FLAGS+=("--labels=zone=den-1,rack=1,host=proxmox")
+fi
 
 # Start the KV server
 ${HOME}/.tiup/components/tikv/v8.2.0/tikv-server "${TIKV_FLAGS[@]}"
@@ -67,4 +85,4 @@ ${HOME}/.tiup/components/tikv/v8.2.0/tikv-server "${TIKV_FLAGS[@]}"
 if [ -n "${PD_PID}" ]; then
 	kill ${PD_PID}
 fi
-rm -rf "${CLUSTER_DIR}"
+rm -rf "${CLUSTER_DIR}/*"
